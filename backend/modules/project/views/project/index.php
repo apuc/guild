0727013ddde3cf9a97ca05f754b4828d5f56391b
Kavalar <apuc06@mail.ru>
2018-12-03 12:14:19 +0300

    fix project index

--- backend/modules/project/views/project/index.php
+++ backend/modules/project/views/project/index.php
@@ -39,7 +39,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 ]),
             ],
             'budget',
-            'description:ntext',
+            //'description:ntext',
             [
                 'label' => 'Исполнители',
                 'value' => function($model){
