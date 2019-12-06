48001992ff6e5c455d816df7e030de5a04aaaa2d
Leorne <leorneq@gmail.com>
2019-12-06 11:30:11 +0300

    view items translate

--- backend/modules/settings/views/additional-fields/_form.php
+++ backend/modules/settings/views/additional-fields/_form.php
@@ -17,7 +17,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'use')->checkboxList((new \common\models\UseField())->statuses); ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
