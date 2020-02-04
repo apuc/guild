dbaeeb3c5a5ca386840a35932561e8dd2f86606b
akosse <andrey_kosse@bk.ru>
2020-02-04 15:23:31 +0300

    link & info in accesses is not required

--- common/models/Accesses.php
+++ common/models/Accesses.php
@@ -40,7 +40,7 @@ class Accesses extends \yii\db\ActiveRecord
             [['info'], 'string'],
             [['_projects'], 'safe'],
             [['_users'], 'safe'],
-            [['name', 'login', 'password', 'link', 'project', 'info'], 'required'],
+            [['name', 'login', 'password', 'project'], 'required'],
         ];
     }
 
